e24fe4144dd16471612dcc02fe51bdbaebc9b211
yschaffler <yannikschaeffler@icloud.com>
2025-10-12 18:19:41 +0200

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const url = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "error", "warn"],
+    datasources: { db: { url } },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
