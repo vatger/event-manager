d28f5d5f4a61e7441a2029e58e9650c5633a850d
yschaffler <yannikschaeffler@icloud.com>
2025-11-16 20:31:51 +0100

    mark read route added

---
+++ app/api/notifications/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import prisma from "@/lib/prisma";
+import { authOptions } from "@/lib/auth";
+
+export async function PATCH(_: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const {id} = await params;
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  const cid = parseInt(session.user.cid);
+
+  try {
+    const ID = parseInt(id);
+    const updated = await prisma.notification.update({
+      where: { id: ID },
+      data: { readAt: new Date() },
+    });
+    return NextResponse.json(updated);
+  } catch (e) {
+    return NextResponse.json({ error: "Update failed" }, { status: 400 });
+  }
+}
