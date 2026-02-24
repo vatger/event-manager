172719082a19aef9eaeb30bea237762c9edd5311
yschaffler <yannikschaeffler@icloud.com>
2026-02-24 19:22:15 +0100

    db fix

--- prisma/migrations/20260224171719_add_weeklysignup_user_realtion/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `WeeklyEventSignup` ADD CONSTRAINT `WeeklyEventSignup_userCID_fkey` FOREIGN KEY (`userCID`) REFERENCES `User`(`cid`) ON DELETE RESTRICT ON UPDATE CASCADE;
